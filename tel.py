f96ee7ec9226ce20c7126dc35d174697b7bf8144
Jasper Timmer <jjwtimmer@gmail.com>
2011-04-23 13:04:39 +0200

    BPS v0.1

--- tel.py
+++ tel.py
@@ -1,33 +1,106 @@
 #!/usr/bin/env python
 from miniboa import TelnetServer
-import urllib2
+import urllib2, time, socket
 
-
+#globals
+JSON_URL = "http://www.batavierenrace.nl/cdb/gpsdata/rvd2011.php"
+GEONAMES_USERNAME = "batapositioning"
 CLIENT_LIST = []
 
+# gps checksum calc
+def chk_chksum(gprmc_str):
+  chk = ''
+  ref = "0x%s" % gprmc_str[-2:]
+  
+  for c in gprmc_str:
+    if c == '*':
+      break
+    elif chk == '':
+      chk = ord(c)
+    else:
+      chk = chk ^ ord(c)
+  
+  return hex(chk).lower() == ref.lower()
+
+#
 def my_on_connect(client):
-    print "client connected: %s" % client.address
-    CLIENT_LIST.append(client)
+  print "client connected: %s" % client.address
+  CLIENT_LIST.append(client)
 
 def my_on_disconnect(client):
-    print "client disconnected: %s" % client.address
-    CLIENT_LIST.remove(client)
+  print "client disconnected: %s" % client.address
+  CLIENT_LIST.remove(client)
     
 def process_clients():
-    for client in CLIENT_LIST:
-        if client.active and client.cmd_ready:
-            data = client.get_command()
-            message = data.split(',')
-            for i in range(0,len(message)-1):
-                print "%d = %s\n" % (i, message[i])
-            url = "http://api.geonames.org/countryCode?lat=%f&lng=%f&username=batapositioning" % (float(message[5])/100,float(message[7])/100)
-            response = urllib2.urlopen(url) 
-            country = response.read()
-            print "Countrycode = %s" % country
+  for client in CLIENT_LIST:
+    if client.active and client.cmd_ready:
+      data = client.get_command()
+      message = data.split(',')
+      str = ','.join(message[2:15])
+      print "%s: IMEI = %s" % (time.gmtime()+2, message[17][6:])
+      if chk_chksum(str):
+        print "GPS Data OK"
+        
+        #===============
+        # begin country retrieval      
+        url = "http://api.geonames.org/countryCode?lat=%f&lng=%f&username=%s" % (float(message[5])/100,float(message[7])/100, GEONAMES_USERNAME)
+        response = urllib2.urlopen(url) 
+        country = response.read()
+        print "Countrycode = %s" % country
+        # end country retrieval
+        #===============
+        
+        
+        #===============
+        # begin battery calculation
+        bat =  float(message[20][2:6]) - 3.65
+        rest = bat / (4.15 - 3.65) * 100
 
-server = TelnetServer(port=9999, address='46.21.169.102', on_connect=my_on_connect, on_disconnect=my_on_disconnect)
+        print "battery = %.1f%%" % rest
+        
+        # end battery calculation
+        #===============
+        
+        #================
+        # start json object building            
+        timestamp = time.strftime("%Y-%m-%dT%H:%M:%SZ", time.gmtime())
+        print "timestamp = %s" % timestamp
 
-print "\n\nStarting server on port %d.  CTRL-C to interrupt.\n" % server.port
-while True:
-    server.poll()
-    process_clients()
+        lat = float(message[5])
+        lon = float(message[7])
+
+        lat = int(lat / 100) + ((lat - (int((lat / 100)) * 100)) / 60)
+        lon = int(lon / 100) + ((lon - (int((lon / 100)) * 100)) / 60)
+        speed = 1.85 * float(message[9])
+
+        output = {}
+        output['id'] = message[17][6:]
+        output['timestamp'] = timestamp
+        output['latitude'] = str(lat)
+        output['longitude'] = str(lon)
+        output['speed'] = str(speed)
+        output['direction'] = message[10]
+
+        enc = json.JSONEncoder(indent = 4)
+        json_out = enc.encode(output)
+           
+        req = urllib2.Request(JSON_URL, json_out)
+        try:
+          response = urllib2.urlopen(req)
+        except HTTPError, e:
+          print e.code
+          print e.read()
+
+        # end json
+        #===============
+        
+      else:
+        print "GPS Data Error"
+#===============================================================
+
+if __name__ == "__main__":
+  server = TelnetServer(port=9999, address=socket.gethostbyname(socket.gethostname()), on_connect=my_on_connect, on_disconnect=my_on_disconnect)
+  print "\n\nStarting server on port %d.  CTRL-C to interrupt.\n" % server.port
+  while True:
+      server.poll()
+      process_clients()
