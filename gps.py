feec7ee23be8b04d9ea0166d757ea2d02c206730
Jasper Timmer <jjwtimmer@gmail.com>
2011-04-29 18:51:13 +0200

    gps fix fix 2

--- gps.py
+++ gps.py
@@ -3,24 +3,7 @@ import datetime, time
 class gps_decoder(object):
   def __init__(self, str):
     self.gpsstring = str
-  
-  # gps checksum calc
-  def check_checksum(self, gprmc_str):
-    chk = ''
-    ref = "0x%s" % gprmc_str[-2:]
     
-    for c in gprmc_str:
-      if c == '*':
-        break
-      elif chk == '':
-        chk = ord(c)
-      else:
-        chk = chk ^ ord(c)
-    
-    return hex(chk).lower() == ref.lower()
-
-  #create gps dict object
-  def get_dict(self):
     data = self.gpsstring.split(',')
 
     gpsdict = {}
@@ -52,4 +35,23 @@ class gps_decoder(object):
     gpsdict['mobile_network_code'] = data[25]
     
     self.dict = gpsdict
-    return gpsdict
+  
+  # gps checksum calc
+  def check_checksum(self):
+    chk = ''
+    ref = "0x%s" % self.dict['gprmc'][-2:]
+    
+    for c in self.dict['gprmc']:
+      if c == '*':
+        break
+      elif chk == '':
+        chk = ord(c)
+      else:
+        chk = chk ^ ord(c)
+    
+    return hex(chk).lower() == ref.lower()
+
+  #create gps dict object
+  def get_dict(self):
+
+    return self.dict
