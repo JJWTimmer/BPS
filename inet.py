440abc955ebb905dbe3268fcd058b2e0d3bcdd33
Jasper Timmer <jjwtimmer@gmail.com>
2012-02-14 00:05:28 +0100

    BPS v2

--- inet.py
+++ inet.py
@@ -2,39 +2,39 @@ import urllib2, json, httplib
 from datetime import timedelta, datetime
 
 class geonames(object):
-  def __init__(self, name):
-    self.name = name
-  
-  def get_country(self, lat, lon):
-    url = "http://api.geonames.org/countryCode?lat=%f&lng=%f&username=%s" % (lat,lon, self.name)
-    response = urllib2.urlopen(url) 
-    country = response.read().strip()
-    return country
- 
+	def __init__(self, name):
+		self.name = name
+	
+	def get_country(self, lat, lon):
+		url = "http://api.geonames.org/countryCode?lat=%f&lng=%f&username=%s" % (lat,lon, self.name)
+		response = urllib2.urlopen(url) 
+		country = response.read().strip()
+		return country
+
 class cdb(object):
-  def __init__(self, url, path):
-    self.url = url
-    self.path = path
-  
-  def post_position(self, dict):
-    #vps is one minute ahead of cdb
-    delta = timedelta(minutes=1)
-    timestamp = (datetime.utcnow()-delta).isoformat() + 'Z'
+	def __init__(self, url, path):
+		self.url = url
+		self.path = path
+	
+	def post_position(self, dict):
+		#vps is one minute ahead of cdb
+		delta = timedelta(minutes=1)
+		timestamp = (datetime.utcnow()-delta).isoformat() + 'Z'
 
-    output = {}
-    output['id'] = dict['imei']
-    output['timestamp'] = timestamp
-    output['latitude'] = "%f" % dict['latitude']
-    output['longitude'] = "%f" % dict['longitude']
-    output['speed'] = "%f" % dict['speed_kmh']
-    output['direction'] = dict['heading']
+		output = {}
+		output['id'] = dict['imei']
+		output['timestamp'] = timestamp
+		output['latitude'] = "%f" % dict['latitude']
+		output['longitude'] = "%f" % dict['longitude']
+		output['speed'] = "%f" % dict['speed_kmh']
+		output['direction'] = dict['heading']
 
-    enc = json.JSONEncoder(indent = 4)
-    json_out = enc.encode(output)
-    
-    headers = {"Content-type": "application/json", "Accept": "application/json; charset=utf8"}
-    conn = httplib.HTTPConnection(self.url)
-    conn.request("POST", self.path, json_out, headers)
-    response = conn.getresponse()
-    
-    return response
+		enc = json.JSONEncoder(indent = 4)
+		json_out = enc.encode(output)
+		
+		headers = {"Content-type": "application/json", "Accept": "application/json; charset=utf8"}
+		conn = httplib.HTTPConnection(self.url)
+		conn.request("POST", self.path, json_out, headers)
+		response = conn.getresponse()
+		
+		return response
--- notification.py
+++ notification.py
@@ -2,21 +2,22 @@ import smtplib
 from email.mime.text import MIMEText
 
 class mailer(object):
-  def __init__(self, smtp_svr, send_addr, recvs):
-    self.smtp_svr = smtp_svr
-    self.send_addr = send_addr
-    self.recvs = recvs.split()
-    
-  def low_battery(self, imei):
-    msg = MIMEText("LOW BATTERY")
-    
-    msg['Subject'] = 'BATTERY OF GPS WITH IMEI %s IS RUNNING LOW' % imei
-    msg['From'] = self.send_addr
-    msg['To'] = ','.join(self.recvs)
+	def __init__(self, smtp_svr, send_addr, recvs):
+		self.smtp_svr = smtp_svr
+		self.send_addr = send_addr
+		self.recvs = recvs.split()
+		
+	def low_battery(self, imei):
+		if len(self.recvs) > 0:
+			msg = MIMEText("LOW BATTERY")
+			
+			msg['Subject'] = 'BATTERY OF GPS WITH IMEI %s IS RUNNING LOW' % imei
+			msg['From'] = self.send_addr
+			msg['To'] = ','.join(self.recvs)
 
-    self.send(msg)
+			self.send(msg)
 
-  def send(self, msg):
-    s = smtplib.SMTP(self.smtp_svr)
-    s.sendmail(self.send_addr, self.recvs, msg.as_string())
-    s.quit()
+	def send(self, msg):
+		s = smtplib.SMTP(self.smtp_svr)
+		s.sendmail(self.send_addr, self.recvs, msg.as_string())
+		s.quit()
