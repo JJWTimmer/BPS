d31721f3dd5c64a38283a6bb3539ff3e5ad98875
Jasper Timmer <jjwtimmer@gmail.com>
2012-03-31 13:58:53 +0200

    Does no longer keep mysql connection open updated README

--- blam.py
+++ blam.py
@@ -2,20 +2,25 @@ import MySQLdb
 
 class blam(object):
 	def __init__(self, host, port, user, pw, db):
-		try:
-			self.db = MySQLdb.connect(host=host, port=port, user=user, passwd=pw, db=db)
-		except MySQLdb.Error, e:
-			raise Exception("DB Connect error: <%s>" % e)
+		self.host = host
+		self.port = port
+		self.user = user
+		self.pw = pw
+		self.db = db
 	
 	def __del__(self):
 		if (self.db):
 			self.db.close()
 	
 	def set_gps_status(self, vehicle_name, status):
+		db = None
 		try:
-			cur = self.db.cursor()
+			db = MySQLdb.connect(host=self.host, port=self.port, user=self.user, passwd=self.pw, db=self.db)
+			cur = db.cursor()
 			cur.execute("UPDATE handles SET gps_status=%d WHERE handle_name='%s' OR description='%s'" % (status, vehicle_name, vehicle_name))
 			
 		except MySQLdb.Error, e:
-			raise Exception("DB Connect error: <%s>" % e)
-	
+			raise Exception("DB error: <%s>" % e)
+		finally:
+			if (db):
+				db.close()
